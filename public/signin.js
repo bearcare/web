d352f57e8c97ba1a4b612302b71067d4d3c5cc38
Jason Haas <jasonrhaas@gmail.com>
2018-02-17 21:46:00 -0700

    Code broken, just committing

--- public/signin.js
+++ public/signin.js
@@ -1,32 +1,46 @@
-import { Connect, SimpleSigner } from 'uport-connect'
-require('dotenv').config()
 
-const uport = new Connect('Bear Care', {
-  clientId: process.env.CLIENT_ID,
-  network: 'rinkeby or ropsten or kovan',
-  signer: SimpleSigner(process.env.SIGNING_KEY)
-})
+// $('#signin').click(function() {
+//   event.preventDefault();
 
-// Request credentials to login
-uport.requestCredentials({
-  requested: ['name', 'phone', 'country'],
-  notifications: true // We want this if we want to recieve credentials
-})
-.then((credentials) => {
-  // Do something
-  console.log('It worked!')
-  console.log(credentials)
-})
-
-// Attest specific credentials
-// uport.attestCredentials({
-//   sub: THE_RECEIVING_UPORT_ADDRESS,
-//   claim: {
-//     CREDENTIAL_NAME: CREDENTIAL_VALUE
-//   },
-//   exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
 // })
 
-// Export web for signing transactions later
-const web3 = uport.getWeb3()
-export { web3, uport, MNID }
+
+// server-side
+function uportConnect () {
+  // Request credentials to login
+  $('#signin').click(function(e) {
+    e.preventDefault()
+    $.ajax({
+        method: "GET",
+        url: "/signin"
+      })
+        // With that done, the data is returned & you can do stuff w/ it...
+        .done(function(data) {
+          // TODO: do something with data object
+          console.log(data);
+      });
+    })
+
+    // Attest specific credentials
+    // uport.attestCredentials({
+    //   sub: THE_RECEIVING_UPORT_ADDRESS,
+    //   claim: {
+    //     CREDENTIAL_NAME: CREDENTIAL_VALUE
+    //   },
+    //   exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
+    // })
+
+    // Export web for signing transactions later
+    // const web3 = uport.getWeb3()
+    // export { web3, uport, MNID }
+
+};
+
+
+var main = function() {
+  uportConnect()
+}
+
+$(document).ready(function() {
+  main()
+})
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+// import { Connect, SimpleSigner } from 'uport-connect'
+require('dotenv').config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var uport = require('uport-connect');
+
+Connect = uport.Connect;
+SimpleSigner = uport.SimpleSigner;
+
 
 // Require all models
 // var db = require("./models");
@@ -28,6 +35,33 @@ app.use(express.static("public"));
 // mongoose.connect(MONGODB_URI, {
 //   useMongoClient: true
 // });
+
+
+app.get("/signin", function(request, response) {
+  // TODO: uPortfunction goes here & returns response? 
+  console.log(process.env.SIGNING_KEY)
+  const uport = new Connect('Bear Care', {
+    clientId: process.env.CLIENT_ID,
+    network: 'rinkeby',
+    signer: SimpleSigner(process.env.SIGNING_KEY)
+  })
+
+  uport.requestCredentials({
+    requested: ['name', 'phone', 'country'],
+    notifications: true // We want this if we want to recieve credentials
+  })
+  .then((credentials) => {
+  	response.send(credentials);
+    // Do something
+    console.log('It worked!')
+    console.log(credentials)
+  })
+   
+});
+
+// 
+
+
 
 // Start the server
 app.listen(PORT, function() {
